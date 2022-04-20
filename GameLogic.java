bf2cb46f0d1bfd0a2293321c2edefb3ee3b28cb0
avrodzz <avrodzz@gmail.com>
2022-04-20 01:49:52 -0700

    Added some instance variables to the GameLogic class. Last commit for Tuesday night/Wednesday morning.

--- GameLogic.java
+++ GameLogic.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GameLogic {
+    private Board board = new Board();
+    private ArrayList<Player> players = new ArrayList<Player>();
+    private Scanner in = new Scanner(System.in); 
+
     public void setUp(){
         System.out.println("GameLogic setUp");
     }
